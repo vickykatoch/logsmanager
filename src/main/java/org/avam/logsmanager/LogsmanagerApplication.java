3c63eea55606741cf5e41ff62bb4a7fe99028e2f
vickykatoch <vickykatoch@yahoo.com>
2020-09-26 07:33:50 -0400

    Initial Checkin

---
+++ src/main/java/org/avam/logsmanager/LogsmanagerApplication.java
@@ -0,0 +1,13 @@
+package org.avam.logsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/avam/logsmanager/LogsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avam.logsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
